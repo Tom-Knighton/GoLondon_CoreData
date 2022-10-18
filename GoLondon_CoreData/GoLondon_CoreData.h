fdb0cae3322326ac8082413658ff37efa5da4935
Tom K <tomknighton@icloud.com>
2022-10-18 21:57:01 +0100

    Initial Commit

---
+++ GoLondon_CoreData/GoLondon_CoreData.h
@@ -0,0 +1,18 @@
+//
+//  GoLondon_CoreData.h
+//  GoLondon_CoreData
+//
+//  Created by Tom Knighton on 18/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoLondon_CoreData.
+FOUNDATION_EXPORT double GoLondon_CoreDataVersionNumber;
+
+//! Project version string for GoLondon_CoreData.
+FOUNDATION_EXPORT const unsigned char GoLondon_CoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoLondon_CoreData/PublicHeader.h>
+
+
